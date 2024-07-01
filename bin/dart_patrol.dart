04bcaa01b79a1183b81089fc31fc429012261019
Brandon Presley <brandon@presley.nz>
2024-07-01 14:29:43 +1200

    Add refactor_tool

---
+++ bin/dart_patrol.dart
@@ -0,0 +1,5 @@
+import 'package:dart_patrol/dart_patrol.dart' as dart_patrol;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_patrol.calculate()}!');
+}
---
+++ test/dart_patrol_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_patrol/dart_patrol.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
